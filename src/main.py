9e73a08c96dc05b4a3e98f2925b7ee551d6b0af1
tony <45546763+pcjitsu@users.noreply.github.com>
2025-03-03 08:23:45 -0600

    Setting up textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType 
+
+def main():
+    newTextnode = TextNode("This is some anchor Text", TextType.LINK, "https://www.google.com")
+    print(newTextnode)
+
+if __name__ == "__main__":
+    main()  # Call main() when the script is run directly
--- src/textnode.py
+++ src/textnode.py
@@ -5,13 +5,19 @@ class TextType(Enum):
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
-    Link = "link"
+    LINK = "link"  # Changed to uppercase for consistency
     IMAGE = "image"
 
-class TextNode(text,text_type,url):
-    def __init__(self,text,text_type,url):
+class TextNode:
+    def __init__(self, text, text_type, url=None):  # Added default value for url
         self.text = text
         self.text_type = text_type
         self.url = url
     
-           
+    def __eq__(self, other):
+        return (self.text == other.text and 
+                self.text_type == other.text_type and 
+                self.url == other.url)
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
